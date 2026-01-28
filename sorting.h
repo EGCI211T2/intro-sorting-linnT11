7768153d8dd46389b63e88b0ffce4af460376be0
linnT11 <linthet.nyi@student.mahidol.edu>
2026-01-28 08:55:50 +0000

    n

--- sorting.h
+++ sorting.h
@@ -1,3 +1,38 @@
 void swap(int &a,int &b);
 void sort(int *a,int N);
 void display(int *a,int N);
+
+
+void swap(int &a,int &b){
+
+
+
+
+}
+
+void sort(int *a,int N){
+
+
+
+
+}
+void display(int *a,int N){
+
+    
+}
+void selectionsort(int *a,int N){
+int i,mi,min,j;
+for (j=0;j<N-1;j++){
+int min=a[j];
+mi=j;
+
+
+    for(i=0;i<N;i++){
+    if(min>a[i]){
+        min =a[i];
+        mi=i;
+    }
+    }
+}
+
+}
